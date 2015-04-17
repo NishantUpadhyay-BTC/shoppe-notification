0be372a27b76c34f248b3d842ee4685cea38d98c
Nishant Upadhyay <nishant.upadhyay@beaverlogic.com>
2015-04-17 19:52:22 +0530

    Update from address

--- app/mailers/shoppe/notification_mailer.rb
+++ app/mailers/shoppe/notification_mailer.rb
@@ -3,13 +3,13 @@ module Shoppe
   
     def order_received(order)
       @order = order
-      mail from: "h.o.bhalani@gmail.com", to: Shoppe.settings.outbound_email_address , subject: "New Order Received"
+      mail from: Shoppe.settings.outbound_email_address, to: Shoppe.settings.outbound_email_address , subject: "New Order Received"
     end
 
     def order_confirmed(order)
       @order = order
       usr_email  = order.email_address
-      mail from: "h.o.bhalani@gmail.com", to: usr_email, subject: "Your Order Confirmed!"
+      mail from: Shoppe.settings.outbound_email_address, to: usr_email, subject: "Your Order Confirmed!"
     end
   end
 end
